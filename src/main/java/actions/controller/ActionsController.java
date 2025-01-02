ec6621e008576214d5c4f45d9e5fc9562c440e9e
Seonho <50858250+bbbng@users.noreply.github.com>
2025-01-02 11:45:42 +0900

    Update ActionsController.java

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -24,7 +24,7 @@ public class ActionsController {
 	
 	@GetMapping(value = "/actions")
 	public String actionsTest() {
-		return "app-with-actions version:0.0.6";
+		return "app-with-actions version:1.0.0";
 	}
 
 	@GetMapping("/students")
